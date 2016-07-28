390756aa0420e598739599c5e18cf2f50d8a1261
Mark Malstrom <malstrommark@gmail.com>
2016-07-28 15:11:01 -0500

    Initial Commit

---
+++ Extractr/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Extractr
+//
+//  Created by Mark Malstrom on 7/28/16.
+//  Copyright © 2016 Tangaroa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Extractr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Extractr
+//
+//  Created by Mark Malstrom on 7/28/16.
+//  Copyright © 2016 Tangaroa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
